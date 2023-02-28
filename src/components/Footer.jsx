41bc3381bc792f8169e9ab08b58cf0a78d7b0892
JuanCruzIses <juancruzises@gmail.com>
2023-02-28 10:56:37 -0300

    Modificaciones en footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import '../css/footer.css'
 import {FaLinkedin} from 'react-icons/fa'
+import {FaGithub} from 'react-icons/fa'
 import {MdEmail} from 'react-icons/md'
+
 
 function Footer(){
     return(
@@ -11,13 +13,15 @@ function Footer(){
                             <FaLinkedin/>
                        </button>
                     </a>
-                    <a href='#header'>
-                        <img className='logo-footer' src={require('../images/logo.png')}></img>
-                    </a>
                     <a  href='https://juancruzises-portfolio.vercel.app/contact'>
                         <button className='footer-button'>
                             <MdEmail/>
                         </button>
+                    </a>
+                    <a rel="noopener noreferrer" target={'_blank'} href='https://github.com/JuanCruzIses'>
+                       <button  className='footer-button'>
+                            <FaGithub/>
+                       </button>
                     </a>
                 </div>
                 <div className='footer-container'>
@@ -25,7 +29,11 @@ function Footer(){
                         <span className='footer-container-title'>JUAN CRUZ ISES</span><br></br>
                         <span>Programador Web Full Stack</span>
                     </div>
-                    <div></div>
+                    <div className='div-container-logo'>
+                    <a href='#header'>
+                        <img className='logo-footer' src={require('../images/logo.png')}></img>
+                    </a>
+                    </div>
                     <div>
                         <span className='footer-container-title'>INFO DE CONTACTO</span><br></br>
                         <span><b>E-mail:</b> juancruzises@gmail.com</span><br></br>
